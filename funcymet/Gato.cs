745c15bbdaaa9392c7785c3d1976100c08a90ff5
bojorquezja <bojorquez.ja@gmail.com>
2024-10-10 08:37:16 -0500

    primer commit

---
+++ funcymet/Gato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcymet {
+    internal class Gato {
+        private string nombre;
+
+        public string GetNombre() {
+            return nombre;
+        }
+        public void SetNombre(string nombre) {
+            this.nombre = nombre;
+        }
+
+        private int edad;
+
+        public int Edad { get => edad; set => edad = value; }
+
+        public string Raza { get; set; }    //trabajaremos con este
+        public string Color { get; set; }    //trabajaremos con este
+        public double Peso { get; set; }    //trabajaremos con este
+    }
+}
---
+++ funcymet/Program.cs
@@ -0,0 +1,11 @@
+﻿using funcymet;
+
+Gato g = new Gato();
+g.SetNombre("Michi");
+g.Edad = 5;
+g.Raza = "Siames";
+
+Console.WriteLine($"Mi gato {g.GetNombre()}, tiene {g.Edad} años y es {g.Raza}");
+
+Gato catnoir = new Gato();
+catnoir.SetNombre("XYZ");
